ed364308aadb7f8fc2be73df0c70c86a81dff960
Abdelmuttalib <hagar9886@gmail.com>
2022-10-27 16:41:30 +0800

    implemented image gallery with supabase

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,10 @@
+import Image from "next/legacy/image";
+import React from "react";
+import cn from "classnames";
+import { createClient } from "@supabase/supabase-js";
+
+
+
+export default Gallery;
+
+
