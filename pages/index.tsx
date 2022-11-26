c762e48225f8d17e1be3a45d2d31324623909346
Abdelmuttalib <hagar9886@gmail.com>
2022-11-27 04:37:10 +0800

    feat(page): add full functinality

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,7 +168,11 @@ const SignIn = ({
 
 import Head from "next/head";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/20/solid";
+import {
+  CheckCircleIcon,
+  XCircleIcon,
+  XMarkIcon,
+} from "@heroicons/react/20/solid";
 import cn from "classnames";
 import { useUser } from "@supabase/auth-helpers-react";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
@@ -184,6 +188,8 @@ type BookT = {
   image_url_m: string;
   image_url_l: string;
   is_available: boolean;
+  genre_id: number;
+  genres: string[];
 };
 
 const Header = () => {
@@ -211,24 +217,54 @@ const Header = () => {
 };
 
 export default function Gallery() {
+  const genres = [
+    "Action & Adventure",
+    "Fantasy",
+    "Science Fiction",
+    "Mystery",
+    "Horror",
+    "Historical Fiction",
+    "Classic",
+    "Poetry",
+    "History",
+  ];
+  const publications = [
+    2004, 2003, 2003, 2002, 2001, 2000, 1999, 1998, 1996, 1995, 1994, 1991,
+    1988, 1979,
+  ];
   const user = useUser();
   const supabaseClient = useSupabaseClient();
   const [books, setBooks] = useState<BookT[]>([]);
   const [search, setSearch] = useState<string>("");
   const [authType, setAuthType] = useState<"sign-in" | "sign-up">("sign-in");
+  const [filterOne, setFilterOne] = useState({
+    type: "",
+    value: "",
+  });
+  const [filterTwo, setFilterTwo] = useState({
+    type: "",
+    value: "",
+  });
+  const [filterThree, setFilterThree] = useState({
+    type: "",
+    value: "",
+  });
 
   useEffect(() => {
     const get = async () => {
       const { data }: { data: BookT[] | null } = await supabaseClient
         .from("books")
         .select("*")
-        .order("id");
+        .order("id")
+        .filter(filterOne.type, "eq", filterOne.value)
+        .filter(filterTwo.type, "eq", filterTwo.value)
+        .filter(filterThree.type, "eq", filterThree.value);
 
       if (data) setBooks(data);
     };
 
     get();
-  }, []);
+  }, [filterOne, filterTwo, filterThree]);
 
   return (
     <>
@@ -239,38 +275,203 @@ export default function Gallery() {
       <Header />
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         {user && (
-          <div>
+          <div className="flex flex-col ">
             <div>
-              <label htmlFor="search"></label>
-              <input
-                type="text"
-                name="search"
-                id="search"
-                className="block w-full bg-gray-100 rounded mb-10 py-4 px-4 text-lg shadow-sm text-[#333] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200"
-                placeholder="Search for book title, author, or year published"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-              />
-            </div>
-            <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {!search &&
-                books &&
-                books.map((book) => <Book key={book.id} {...book} />)}
+              <div>
+                <label htmlFor="search"></label>
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  className="block w-full bg-gray-100 rounded mb-5 py-4 px-4 text-lg shadow-sm text-[#333] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200"
+                  placeholder="Search for book title, author, or year published"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+              </div>
+              {/* Filters */}
+              <div className="flex flex-wrap gap-5">
+                <div className="flex items-center gap-2">
+                  <span className="font-medium">Genre:</span>
+                  <select
+                    name="genre"
+                    id="genre"
+                    className="block w-fit bg-gray-100 hover:bg-gray-200 ease-linear duration-150 cursor-pointer rounded py-2 px-4 text-lg shadow-sm text-[#333] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200"
+                    value={filterOne.value}
+                    onChange={(e) => {
+                      if (e.target.value === "") {
+                        setFilterOne({
+                          type: "",
+                          value: "",
+                        });
+                      } else {
+                        setFilterOne({
+                          type: "genre_id",
+                          value: e.target.value,
+                        });
+                      }
+                    }}
+                  >
+                    <option key={1} value="">
+                      select...
+                    </option>
+                    {genres.map((genre, indx) => (
+                      <option key={genre} value={indx + 1}>
+                        {genre}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span className="font-medium">Publication Year:</span>
+                  <select
+                    name="publication"
+                    id="publication"
+                    className="block w-fit bg-gray-100 hover:bg-gray-200 ease-linear duration-150 cursor-pointer rounded py-2 px-4 text-lg shadow-sm text-[#333] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200"
+                    value={filterTwo.value}
+                    onChange={(e) => {
+                      if (e.target.value === "") {
+                        setFilterTwo({
+                          type: "",
+                          value: "",
+                        });
+                      } else {
+                        setFilterTwo({
+                          type: "publication",
+                          value: e.target.value,
+                        });
+                      }
+                    }}
+                  >
+                    <option key={1} value="">
+                      select...
+                    </option>
+                    {publications.map((p, indx) => (
+                      <option key={p} value={p}>
+                        {p}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="flex items-center gap-2">
+                  <span className="font-medium">Availability:</span>
+                  <select
+                    name="genre"
+                    id="genre"
+                    className="block w-fit bg-gray-100 hover:bg-gray-200 ease-linear duration-150 cursor-pointer rounded py-2 px-4 text-lg shadow-sm text-[#333] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200"
+                    value={filterThree.value}
+                    onChange={(e) => {
+                      if (e.target.value === "") {
+                        setFilterThree({
+                          type: "",
+                          value: "",
+                        });
+                      } else {
+                        setFilterThree({
+                          type: "is_available",
+                          value: e.target.value,
+                        });
+                      }
+                    }}
+                  >
+                    <option key={1} value="">
+                      select...
+                    </option>
+                    <option key={2} value="true">
+                      Available
+                    </option>
+                    <option key={3} value="false">
+                      Not Available
+                    </option>
+                  </select>
+                </div>
+              </div>
 
-              {search &&
-                books &&
-                books
-                  .filter(
-                    (book) =>
-                      book.author
-                        .toLowerCase()
-                        .includes(search.toLowerCase()) ||
-                      book.book_title
-                        .toLowerCase()
-                        .includes(search.toLowerCase()) ||
-                      book.publication.toString().includes(search)
-                  )
-                  .map((book) => <Book key={book.id} {...book} />)}
+              {/* b */}
+              {filterOne.value || filterTwo.value || filterThree.value ? (
+                <div className="mt-6 flex gap-2 items-center">
+                  <p className="text-gray-600">Filters: </p>
+
+                  {filterOne.value && (
+                    <button
+                      type="button"
+                      className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-gray-200 text-gray-800"
+                    >
+                      {genres[parseInt(filterOne.value) - 1]}
+                      <XCircleIcon
+                        onClick={() =>
+                          setFilterOne({
+                            type: "",
+                            value: "",
+                          })
+                        }
+                        className="w-5 text-gray-600 ml-3 hover:text-gray-700"
+                      />
+                    </button>
+                  )}
+
+                  {filterTwo.value && (
+                    <button
+                      type="button"
+                      className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-gray-200 text-gray-800"
+                    >
+                      {filterTwo.value}
+                      <XCircleIcon
+                        onClick={() =>
+                          setFilterTwo({
+                            type: "",
+                            value: "",
+                          })
+                        }
+                        className="w-5 text-gray-600 ml-3 hover:text-gray-700"
+                      />
+                    </button>
+                  )}
+
+                  {filterThree.value && (
+                    <button
+                      type="button"
+                      className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-gray-200 text-gray-800"
+                    >
+                      {filterThree.value ? "Available" : "Not Available"}
+                      <XCircleIcon
+                        onClick={() =>
+                          setFilterThree({
+                            type: "",
+                            value: "",
+                          })
+                        }
+                        className="w-5 text-gray-600 ml-3 hover:text-gray-700"
+                      />
+                    </button>
+                  )}
+                </div>
+              ) : null}
+              {/* ./Filters */}
+              <div className="mt-8 grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                {!search &&
+                  books &&
+                  books.map((book) => (
+                    <Book key={book.id} {...book} genres={genres} />
+                  ))}
+
+                {search &&
+                  books &&
+                  books
+                    .filter(
+                      (book) =>
+                        book.author
+                          .toLowerCase()
+                          .includes(search.toLowerCase()) ||
+                        book.book_title
+                          .toLowerCase()
+                          .includes(search.toLowerCase()) ||
+                        book.publication.toString().includes(search)
+                    )
+                    .map((book) => (
+                      <Book key={book.id} {...book} genres={genres} />
+                    ))}
+              </div>
             </div>
           </div>
         )}
@@ -292,6 +493,8 @@ const Book = ({
   publication,
   image_url_l,
   is_available,
+  genre_id,
+  genres,
 }: BookT) => {
   return (
     <div className="h-fit">
@@ -302,9 +505,13 @@ const Book = ({
         <div className="flex justify-between items-center mt-4">
           <h3 className="text-base font-medium text-gray-900">{book_title}</h3>
         </div>
-        <p className="mt-1 text-sm text-gray-500">
+        <p className="mt-1 text-sm text-gray-500 font-medium">
           {publication} by {author}
         </p>
+
+        <span className="text-sm block underline text-gray-400">
+          Genre: {genres[genre_id - 1]}
+        </span>
 
         <div className="mt-2">
           {is_available ? (
