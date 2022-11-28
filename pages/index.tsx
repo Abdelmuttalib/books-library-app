3b817264f80f77836014fd9811d4653a4c12eefa
Abdelmuttalib <hagar9886@gmail.com>
2022-11-28 22:42:58 +0800

    add spaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -450,7 +450,7 @@ export default function Gallery() {
                 </div>
               ) : null}
               {/* ./Filters */}
-              <div className="mt-8 grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              <div className="mt-14 grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 {!search &&
                   books &&
                   books.map((book) => (
